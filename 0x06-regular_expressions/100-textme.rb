f67eab3572c150072828da7463bfb5e5a8016202
HazelDaniel <hazeldaniel856@gmail.com>
2023-08-01 09:23:36 +0100

    submission for task 100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 
 str = ARGV[0]
-pattern = /(?<=\[from:)([^\]]+)\]\s*.*(?<=\[to:)([^\]]+)\]\s*.*(?<=\[flags:)([^\]]+)\]/
-output = str.scan(pattern)
-output.each do |match|
-  puts "#{match[0]}"",""#{match[1]}"",""#{match[2]}"
-end
+pattern = /(?:(?<=\[from:)|(?<=\[to:)|(?<=\[flags:))([^\]]+)\]/
+output = str.scan(pattern).join(",")
+puts output
