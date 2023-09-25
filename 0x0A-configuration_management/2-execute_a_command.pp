551cbed515b4bf14ab3be68b3a167b0c457d4540
HazelDaniel <hazeldaniel856@gmail.com>
2023-09-25 10:50:33 +0100

    re-submission for task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #this executes a command to stop a process in puppet
 
 exec { 'kill killmenow':
-  command => 'pkill -9 ./killmenow',
+  command => 'pkill -9 -f killmenow',
   path    => ['/usr/bin', '/usr/sbin', '/bin']
 }
