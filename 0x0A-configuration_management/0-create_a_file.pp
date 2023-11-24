c4206bc9ee699ea6263e1547a202a2cd35c368dc
HazelDaniel <hazeldaniel856@gmail.com>
2023-11-24 23:51:01 +0100

    task-0: re-submission

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # this is a puppet manifest responsible for creating a file called "school" in the /tmp directory
 
-file { "school":
-	ensure => file,
-	require => "/tmp",
-	group => "www-data",
-	owner => "www-data",
-	content => "I love Puppet",
+file { 'school':
+  ensure  => file,
+  group   => 'www-data',
+  owner   => 'www-data',
+  content => 'I love Puppet',
 }
