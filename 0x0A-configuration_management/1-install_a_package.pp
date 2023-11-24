790799dcbec54ee2dcfbe1c7e4e1d6fafa01d634
HazelDaniel <hazeldaniel856@gmail.com>
2023-11-25 00:00:25 +0100

    task-1: submission

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# this puppet manifest installs flask 2.1.0 from pip3
+
+file {'/usr/bin/pip3':
+  ensure => file,
+}
+
+exec {'flask-install':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  require => File['/usr/bin/pip3'],
+  unless  => '/usr/bin/pip3 show Flask'
+}
